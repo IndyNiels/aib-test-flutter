df7a204e9a2f33e4e060bc96fafa2199860bd624
Indy Niels Higuet <indyhiguet94@gmail.com>
2023-08-25 14:00:45 +0200

    basic formatting

--- lib/features/pokemon/presentation/pokemon_details/pages/pokemon_details_page.dart
+++ lib/features/pokemon/presentation/pokemon_details/pages/pokemon_details_page.dart
@@ -6,6 +6,7 @@ import 'package:aib_test/features/pokemon/domain/model/pokemon/pokemon.dart';
 import 'package:aib_test/features/pokemon/domain/repository/pokemons_repository.dart';
 import 'package:aib_test/features/pokemon/presentation/pokemon_details/cubit/pokemon_details_cubit.dart';
 import 'package:aib_test/features/pokemon/presentation/pokemon_details/cubit/pokemon_details_state.dart';
+import 'package:aib_test/core/utils/helpers.dart';
 
 class PokemonDetails extends StatelessWidget {
   const PokemonDetails({
@@ -43,19 +44,52 @@ class PokemonDetailsContent extends StatelessWidget {
       builder: (context, state) {
         if (state.status.isSuccess) {
           return ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             children: [
-              Text(
-                'Nombre: ${state.pokemon?.name ?? "N/A"}',
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Text('ID: ${state.pokemon?.id ?? "N/A"}'),
-              Text('ID: ${state.pokemon  ?? "N/A"}'),
               Image.network(
                 state.pokemon?.sprites?.backDefault ?? '',
                 width: 150,
+                height: 150,
               ),
+              Align(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text(
+                      capitalizeFirstLetter(state.pokemon?.name ?? 'N/A'),
+                      style: const TextStyle(
+                          fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '#00${state.pokemon?.id ?? 'N/A'}',
+                      style: const TextStyle(
+                        color: Color(0xFFCBCBCB),
+                        fontFamily: 'Roboto',
+                        fontSize: 12,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w400,
+                        height:
+                            1.66667, // Calculated from line-height percentage
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Text(
+                'Description',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16), // Adding vertical spacing
+              Text(
+                  ' Nulla eget ante nulla. Sed placerat auctor sagittis. Suspendisse semper, urna in blandit auctor, lorem lectus posuere massa, vitae laoreet orci nunc eget magna. Fusce nec venenatis urna. In varius eros eget odio pretium, eu tincidunt est dictum.',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    height: 1.57143,
+                  )),
+              Spacer(),
+              // Text('ID: ${state.pokemon  ?? "N/A"}'),
             ],
           );
         }
